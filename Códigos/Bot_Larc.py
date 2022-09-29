9636aa304d4863a1ba373d462b1a6884ef001918
luisafrancielle <108543034+luisafrancielle@users.noreply.github.com>
2022-09-29 16:24:02 -0300

    Update Bot_Larc.py

--- Códigos/Bot_Larc.py
+++ Códigos/Bot_Larc.py
@@ -81,24 +81,24 @@ class Bot():
                 bot.mover.parar()
                 bot.mover.girar(dir*5)
 
-def acha_vazio(bot):
-    bot.mover.frente()
-    while True:
-        sleep(0.1)
-        if bot.cor_dir.color == 0 or bot.cor_esq.color == 0:
-            bot.mover.parar()
-            return
+    def acha_vazio(bot):
+        bot.mover.frente()
+        while True:
+            sleep(0.1)
+            if bot.cor_dir.color == 0 or bot.cor_esq.color == 0:
+                bot.mover.parar()
+                return
             
         
-def re_vazio(bot):
-    bot.mover.tras(5)
-    while True:
-        sleep(0.2)
-        if bot.cor_dir.color == 0 and bot.cor_esq.color == 0: continue
-        elif bot.cor_dir.color == 0: return -1
-        elif bot.cor_esq.color == 0: return 1
-        else: 
-            return 0
+    def re_vazio(bot):
+        bot.mover.tras(5)
+        while True:
+            sleep(0.2)
+            if bot.cor_dir.color == 0 and bot.cor_esq.color == 0: continue
+            elif bot.cor_dir.color == 0: return -1
+            elif bot.cor_esq.color == 0: return 1
+            else: 
+                return 0
 
 if __name__== '__main__':
     bot = Bot()
