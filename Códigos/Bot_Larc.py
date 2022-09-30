4b6b404c0459dd861ad6acf9fec0af69a76ba4af
Luísa Francielle <108543034+luisafrancielle@users.noreply.github.com>
2022-09-30 14:53:04 -0300

    Update Bot_Larc.py

--- Códigos/Bot_Larc.py
+++ Códigos/Bot_Larc.py
@@ -70,36 +70,7 @@ class Bot():
         else:
             return self.ultsnc_lado.distance_centimeters
 
- class Alinhamento():
-    def __init__(self, bot): 
-        while True:
-                acha_vazio(bot)
-                dir = re_vazio(bot)
-                if dir == 0: return
-                bot.mover.tras()
-                sleep(1)
-                bot.mover.parar()
-                bot.mover.girar(dir*5)
-
-    def acha_vazio(bot):
-        bot.mover.frente()
-        while True:
-            sleep(0.1)
-            if bot.cor_dir.color == 0 or bot.cor_esq.color == 0:
-                bot.mover.parar()
-                return
-            
-        
-    def re_vazio(bot):
-        bot.mover.tras(5)
-        while True:
-            sleep(0.2)
-            if bot.cor_dir.color == 0 and bot.cor_esq.color == 0: continue
-            elif bot.cor_dir.color == 0: return -1
-            elif bot.cor_esq.color == 0: return 1
-            else: 
-                return 0
-
+ 
 if __name__== '__main__':
     bot = Bot()
     bot.mover.tras()
